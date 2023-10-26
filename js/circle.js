2d4b3adea40c32fcd60940a172fdd82e063f273d
Ricardo Jaimes Hernandez <ricardo.jaimes.h@gmail.com>
2023-10-26 07:01:21 -0700

    completed objects exercises

--- js/circle.js
+++ js/circle.js
@@ -1,37 +1,41 @@
 (function() {
     "use strict";
 
-    // create a circle object
+    // Create a circle object
     var circle = {
         radius: 3,
 
         getArea: function () {
-            // TODO: complete this method
-            // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+            // Calculate and return the area
+            return Math.PI * this.radius * this.radius;
         },
 
         logInfo: function (doRounding) {
-            // TODO: complete this method.
+            // Get the area of the circle
+            let area = this.getArea();
 
             // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
+            if (doRounding) {
+                area = Math.round(area);
+            }
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            // Output the circle information
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
         }
     };
 
-    // log info about the circle
+    // Log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
 
     console.log("=======================================================");
-    // TODO: Change the radius of the circle to 5.
 
-    // log info about the circle
+    // Change the radius of the circle to 5.
+    circle.radius = 5;
+
+    // Log info about the circle with the updated radius
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");

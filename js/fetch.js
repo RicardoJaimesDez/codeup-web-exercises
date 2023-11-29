fda1b0b5535c58ceff2c584f896980cc3d7dc383
Ricardo Jaimes Hernandez <ricardo.jaimes.h@gmail.com>
2023-11-29 07:45:10 -0800

    completed Handle An API Promise Exercise

--- js/fetch.js
+++ js/fetch.js
@@ -2,13 +2,31 @@
 
 (() => {
     // console.log(GH_KEY);
-    const username = 'RicardoJaimesDez';
-    const url = `https://api.github.com/users/${username}/events`;
-    fetch(url, {headers: {'Authorization': `${GH_KEY}`}})
-        .then(response => response.json())
-        .then(data => console.log(data));
 
+    function getLastCommitDate(username) {
+        const url = `https://api.github.com/users/${username}/events`;
 
+        return new Promise((resolve, reject) => {
+            fetch(url, {headers: {'Authorization': `${GH_KEY}`}})
+                .then(response => {
+                    if (response) {
+                        return response.json();
+                    }
+                })
+                .then(events => {
+                    const lastCommitEvent = events.find(event => event.type === 'PushEvent');
+                    if (lastCommitEvent) {
+                        const lastCommitDate = new Date(lastCommitEvent["created_at"]);
+                        resolve(lastCommitDate.toDateString());
+                    }
+                });
 
+        });
+    }
 
+    const githubUsername = 'RicardoJaimesDez';
+    getLastCommitDate(githubUsername)
+        .then(date => {
+            console.log(`Last commit date for ${githubUsername} was on: ${date}`);
+        });
 })();

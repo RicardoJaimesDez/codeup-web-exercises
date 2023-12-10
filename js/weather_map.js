bf812a4c07a387218bd339152284df39d231081d
Ricardo Jaimes Hernandez <ricardo.jaimes.h@gmail.com>
2023-12-10 08:30:15 -0800

    fixed the div container that holds the five day cards

--- js/weather_map.js
+++ js/weather_map.js
@@ -24,7 +24,7 @@
         })
         .catch(error => alert(`Error fetching weather data: ${error.message}`));
     }
-currentWeatherInCorona();
+
 
     // 5-Day Forecast Function
     function fiveDayForecast() {
@@ -36,17 +36,61 @@ currentWeatherInCorona();
                 console.log(data)
                 let forecastData = '';
                 for (let i = 0; i < data.list.length; i += 8) {
-                    const fiveDayWeatherForecast = data.list[i].main.temp;
+                    const name = data.city.name;
+                    const date = new Date(data.list[i].dt * 1000).toDateString();
+                    const temp = data.list[i].main.temp;
+                    const lowTemp = data.list[i].main.temp_min;
+                    const highTemp = data.list[i].main.temp_max;
+                    const description = data.list[i].weather[0].description;
+                    const humidity = data.list[i].main.humidity;
+                    const wind = data.list[i].wind.speed;
+                    const pressure = data.list[i].main.pressure;
+                    console.log(date);
+                    // console.log(name);
+                    // console.log(pressure);
+                    // console.log(wind)
+                    // console.log(humidity);
+                    // console.log(description);
+                    // console.log(temp);
+                    // console.log(lowTemp);
+                    // console.log(highTemp);
                     // console.log(data.list[i].main.temp);
-                    forecastData += `The Five day forecast weather is: ${fiveDayWeatherForecast} °F<br>`;
+                    // forecastData += `The Five day forecast weather is: ${temp} °F<br>`;
+                    forecastData += `
+                          <div class="col">
+                            <div class="card h-100">
+<!--                                              <img src="..." class="card-img-top" alt="...">-->
+                              <div class="card-body">
+                                <h4 class="card-title">${date}</h4>
+                                <h5>Current Temperature: ${temp} °F</h5>
+                                <h6>${lowTemp} °F / ${highTemp} °F</h6>
+    <!--    Add AN IMAGE HERE THAT COROESPONDS WITH THE DESCRIPTION -->
+                                <p class="card-text">Description: ${description}</p>
+                                <p class="card-text">Humidity: ${humidity}</p>
+                                <p class="card-text">Wind: ${wind}</p>
+                                <p class="card-text">Pressure: ${pressure}</p>
+                              </div>
+                            </div>
+                          </div>`;
                 }
                 const fiveDayWeather = document.querySelector("#fiveDayForecast");
                 fiveDayWeather.innerHTML = forecastData;
             });
      }
 
-fiveDayForecast();
 
 
+    //-------------------------------------------- THIS IS THE CODE FOR MAPBOX -----------------------------------------
+    mapboxgl.accessToken = MAPBOX_KEY;
 
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: [-117.5664, 33.8753], // starting position [lng, lat] Corona, CA.
+        zoom: 11 // starting zoom
+    });
+    // map.addControl(new mapboxgl.NavigationControl());
+
+    currentWeatherInCorona();
+    fiveDayForecast();
 })();
